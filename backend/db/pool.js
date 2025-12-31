f4049e2a6750186f285143fe7ab718d43a09db2a
JUNIORPRUEVA <jrdigitalrd@gmail.com>
2025-12-31 12:42:06 -0400

    Fix: deps + pool export for EasyPanel

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -1,4 +1,4 @@
-const { Pool } = require('pg');
+﻿const { Pool } = require('pg');
 
 function createPool() {
   const connectionString = process.env.DATABASE_URL;
@@ -33,4 +33,9 @@ function createPool() {
 
 const pool = createPool();
 
-module.exports = { pool };
+// Compat: soporta ambos estilos de importación en el código:
+//   const pool = require('../db/pool')
+//   const { pool } = require('../db/pool')
+module.exports = pool;
+module.exports.pool = pool;
+
